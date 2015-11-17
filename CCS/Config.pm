09e82fd86955fc074bdca52812264c7edebd5365
Bryan Jurish <jurish@bbaw.de>
2015-11-17 09:09:27 +0000

    + updated to v1.22.4
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/diss-perl/trunk/PDL-CCS@2370 097f6656-c427-11dc-a790-1b5d5187385a

--- CCS/Config.pm
+++ CCS/Config.pm
@@ -9,18 +9,9 @@ our @EXPORT_OK   = ('%ccsConfig', 'ccs_indx');
 our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, all=>\@EXPORT_OK);
 
 %ccsConfig = (
-               'INDX_SIG' => 'indx',
-               'INDX_TYPEDEF' => 'typedef PDL_Indx CCS_Indx;  /**< typedef for CCS indices */
+               'INDX_FUNCDEF' => '*ccs_indx = \\&PDL::indx; ##-- typecasting for CCS indices
 ',
-               'INDX_FUNC' => 'indx',
-               'INT_TYPE_KEYS' => [
-                                    'PDL_B',
-                                    'PDL_IND',
-                                    'PDL_L',
-                                    'PDL_LL',
-                                    'PDL_S',
-                                    'PDL_US'
-                                  ],
+               'INDX_CTYPE' => 'PDL_Indx',
                'INT_TYPE_CHRS' => [
                                     'B',
                                     'L',
@@ -29,10 +20,19 @@ our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, a
                                     'S',
                                     'U'
                                   ],
-               'INDX_FUNCDEF' => '*ccs_indx = \\&PDL::indx; ##-- typecasting for CCS indices
+               'INDX_TYPEDEF' => 'typedef PDL_Indx CCS_Indx;  /**< typedef for CCS indices */
 ',
+               'INDX_SIG' => 'indx',
+               'INDX_FUNC' => 'indx',
                'USE_PDL_INDX' => 1,
-               'INDX_CTYPE' => 'PDL_Indx'
+               'INT_TYPE_KEYS' => [
+                                    'PDL_B',
+                                    'PDL_IND',
+                                    'PDL_L',
+                                    'PDL_LL',
+                                    'PDL_S',
+                                    'PDL_US'
+                                  ]
              );
 
 *PDL::ccs_indx = *ccs_indx = \&PDL::indx; ##-- typecasting for CCS indices
--- CCS/Version.pm
+++ CCS/Version.pm
@@ -3,6 +3,6 @@
 ## Description: set version for PDL::CCS
 
 package PDL::CCS::Version;
-our $VERSION = $PDL::CCS::VERSION = '1.22.3';
+our $VERSION = $PDL::CCS::VERSION = '1.22.4';
 
 1; ##-- make perl happy
