33db5aee041caefcc37eba5be81f802ff1d5dfc9
Bryan Jurish <jurish@bbaw.de>
2015-11-17 09:03:09 +0000

    v1.22.4: Makefile.PL workaround for PDL::VectorValued(::Version) strangeness on cpantesters
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/diss-perl/trunk/PDL-CCS@2369 097f6656-c427-11dc-a790-1b5d5187385a

--- CCS/Config.pm
+++ CCS/Config.pm
@@ -9,17 +9,10 @@ our @EXPORT_OK   = ('%ccsConfig', 'ccs_indx');
 our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, all=>\@EXPORT_OK);
 
 %ccsConfig = (
-               'USE_PDL_INDX' => 1,
+               'INDX_SIG' => 'indx',
                'INDX_TYPEDEF' => 'typedef PDL_Indx CCS_Indx;  /**< typedef for CCS indices */
 ',
-               'INT_TYPE_CHRS' => [
-                                    'B',
-                                    'L',
-                                    'N',
-                                    'Q',
-                                    'S',
-                                    'U'
-                                  ],
+               'INDX_FUNC' => 'indx',
                'INT_TYPE_KEYS' => [
                                     'PDL_B',
                                     'PDL_IND',
@@ -28,11 +21,18 @@ our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, a
                                     'PDL_S',
                                     'PDL_US'
                                   ],
-               'INDX_FUNC' => 'indx',
-               'INDX_CTYPE' => 'PDL_Indx',
-               'INDX_SIG' => 'indx',
+               'INT_TYPE_CHRS' => [
+                                    'B',
+                                    'L',
+                                    'N',
+                                    'Q',
+                                    'S',
+                                    'U'
+                                  ],
                'INDX_FUNCDEF' => '*ccs_indx = \\&PDL::indx; ##-- typecasting for CCS indices
-'
+',
+               'USE_PDL_INDX' => 1,
+               'INDX_CTYPE' => 'PDL_Indx'
              );
 
 *PDL::ccs_indx = *ccs_indx = \&PDL::indx; ##-- typecasting for CCS indices
