b6d0c5631db40f3fed5678d04d7e2d91c752303d
Bryan Jurish <jurish@bbaw.de>
2015-11-26 15:19:44 +0000

    + MANIFEST + ChangeLog
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/diss-perl/trunk/PDL-CCS@2391 097f6656-c427-11dc-a790-1b5d5187385a

--- CCS/Config.pm
+++ CCS/Config.pm
@@ -9,6 +9,7 @@ our @EXPORT_OK   = ('%ccsConfig', 'ccs_indx');
 our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, all=>\@EXPORT_OK);
 
 %ccsConfig = (
+               'INDX_CTYPE' => 'PDL_Indx',
                'INT_TYPE_CHRS' => [
                                     'B',
                                     'L',
@@ -17,14 +18,9 @@ our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, a
                                     'S',
                                     'U'
                                   ],
-               'USE_PDL_INDX' => 1,
                'INDX_FUNCDEF' => '*ccs_indx = \\&PDL::indx; ##-- typecasting for CCS indices
 ',
-               'INDX_TYPEDEF' => 'typedef PDL_Indx CCS_Indx;  /**< typedef for CCS indices */
-',
-               'INDX_CTYPE' => 'PDL_Indx',
                'INDX_FUNC' => 'indx',
-               'INDX_SIG' => 'indx',
                'INT_TYPE_KEYS' => [
                                     'PDL_B',
                                     'PDL_IND',
@@ -32,7 +28,11 @@ our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, a
                                     'PDL_LL',
                                     'PDL_S',
                                     'PDL_US'
-                                  ]
+                                  ],
+               'INDX_TYPEDEF' => 'typedef PDL_Indx CCS_Indx;  /**< typedef for CCS indices */
+',
+               'USE_PDL_INDX' => 1,
+               'INDX_SIG' => 'indx'
              );
 
 *PDL::ccs_indx = *ccs_indx = \&PDL::indx; ##-- typecasting for CCS indices
