ae96d39886f48e4d479f7a7474e05f9facd8f344
Bryan Jurish <jurish@bbaw.de>
2015-12-15 13:03:29 +0000

    + ignore (ccs|dense)3.* in CCS/IO/t/
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/diss-perl/trunk/PDL-CCS@2425 097f6656-c427-11dc-a790-1b5d5187385a

--- CCS/Config.pm
+++ CCS/Config.pm
@@ -9,13 +9,20 @@ our @EXPORT_OK   = ('%ccsConfig', 'ccs_indx');
 our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, all=>\@EXPORT_OK);
 
 %ccsConfig = (
+               'INDX_FUNCDEF' => '*ccs_indx = \\&PDL::indx; ##-- typecasting for CCS indices
+',
                'INDX_TYPEDEF' => 'typedef PDL_Indx CCS_Indx;  /**< typedef for CCS indices */
 ',
                'INDX_FUNC' => 'indx',
-               'INDX_FUNCDEF' => '*ccs_indx = \\&PDL::indx; ##-- typecasting for CCS indices
-',
-               'INDX_SIG' => 'indx',
-               'USE_PDL_INDX' => 1,
+               'INT_TYPE_KEYS' => [
+                                    'PDL_B',
+                                    'PDL_S',
+                                    'PDL_US',
+                                    'PDL_L',
+                                    'PDL_IND',
+                                    'PDL_LL'
+                                  ],
+               'INDX_CTYPE' => 'PDL_Indx',
                'INT_TYPE_CHRS' => [
                                     'B',
                                     'S',
@@ -24,15 +31,8 @@ our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, a
                                     'N',
                                     'Q'
                                   ],
-               'INDX_CTYPE' => 'PDL_Indx',
-               'INT_TYPE_KEYS' => [
-                                    'PDL_B',
-                                    'PDL_S',
-                                    'PDL_US',
-                                    'PDL_L',
-                                    'PDL_IND',
-                                    'PDL_LL'
-                                  ]
+               'INDX_SIG' => 'indx',
+               'USE_PDL_INDX' => 1
              );
 
 *PDL::ccs_indx = *ccs_indx = \&PDL::indx; ##-- typecasting for CCS indices
