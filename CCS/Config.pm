fc1d4ca89ebb54f0fcb016a7ebd0ac6ff57313b7
Bryan Jurish <jurish@bbaw.de>
2018-06-05 14:41:10 +0000

    + v1.23.5: fixed typos reported by L. Baillet (RT bug #125493)
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/diss-perl/trunk/PDL-CCS@2521 097f6656-c427-11dc-a790-1b5d5187385a

--- CCS/Config.pm
+++ CCS/Config.pm
@@ -9,12 +9,10 @@ our @EXPORT_OK   = ('%ccsConfig', 'ccs_indx');
 our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, all=>\@EXPORT_OK);
 
 %ccsConfig = (
-               'INDX_CTYPE' => 'PDL_Indx',
-               'INDX_TYPEDEF' => 'typedef PDL_Indx CCS_Indx;  /**< typedef for CCS indices */
+               'INDX_FUNCDEF' => '*ccs_indx = \\&PDL::indx; ##-- typecasting for CCS indices
 ',
-               'INDX_FUNC' => 'indx',
+               'INDX_CTYPE' => 'PDL_Indx',
                'INDX_SIG' => 'indx',
-               'USE_PDL_INDX' => 1,
                'INT_TYPE_CHRS' => [
                                     'B',
                                     'S',
@@ -23,8 +21,6 @@ our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, a
                                     'N',
                                     'Q'
                                   ],
-               'INDX_FUNCDEF' => '*ccs_indx = \\&PDL::indx; ##-- typecasting for CCS indices
-',
                'INT_TYPE_KEYS' => [
                                     'PDL_B',
                                     'PDL_S',
@@ -32,7 +28,11 @@ our %EXPORT_TAGS = (config=>['%ccsConfig'], Func=>\@Export, default=>\@EXPORT, a
                                     'PDL_L',
                                     'PDL_IND',
                                     'PDL_LL'
-                                  ]
+                                  ],
+               'INDX_TYPEDEF' => 'typedef PDL_Indx CCS_Indx;  /**< typedef for CCS indices */
+',
+               'INDX_FUNC' => 'indx',
+               'USE_PDL_INDX' => 1
              );
 
 *PDL::ccs_indx = *ccs_indx = \&PDL::indx; ##-- typecasting for CCS indices
--- CCS/IO/Common.pm
+++ CCS/IO/Common.pm
@@ -9,7 +9,7 @@ use PDL;
 use Carp qw(confess);
 use strict;
 
-our $VERSION = '1.23.5';
+our $VERSION = '1.23.6';
 our @ISA = ('PDL::Exporter');
 our @EXPORT_OK =
   (
